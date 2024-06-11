a5af33aed107ff53ca521106fec58ca6048d6c76
Xiaodoubi2333 <crazysnail2133@gmail.com>
2024-06-11 11:30:54 +0800

    成品.

--- app/src/main/java/com/coolweather/android/ui/readmework/ReadmeworkViewModel.java
+++ app/src/main/java/com/coolweather/android/ui/readmework/ReadmeworkViewModel.java
@@ -15,10 +15,10 @@ public class ReadmeworkViewModel extends ViewModel {
 
         mText.setValue("作品简介");
         mDescription.setValue("应用：SQLite，网络天气APP.\n" +
-                "一、本应用使用底部导航，方便使用。\n" +
-                "二、应用还涉及：网络JSON请求( 调用天气api)、天气信息显示、天气收藏夹\n\n" +
-                "1.调用天气api：使用chooseAreaFragment，解析JSON；返回全局存储：countyName、weather_id \n\n" +
-                "2.显示天气：全局存储SharedPreferences实例，调用，启动天气显示\n\n" +
+                "一、本应用使用底部导航，方便使用。\n\n" +
+                "二、应用还涉及：网络JSON请求( 调用天气api)、天气信息显示、天气收藏夹\n" +
+                "1.调用天气api：使用chooseAreaFragment，解析JSON；返回全局存储：countyName、weather_id\n" +
+                "2.显示天气：全局存储SharedPreferences实例，调用，启动天气显示\n" +
                 "3.天气收藏夹：使用EventBus库实现控件交互通信、内容提供者组件显示天气收藏夹.\n\n" +
                 "三、综合：使用内容提供者组件、EventBus库进行组件间通信、ListView控件、BaseAdapter适配器实现收藏夹列表，" +
                 "并使用SweetAlertDialog进行弹窗删除警告。");
